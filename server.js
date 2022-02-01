b88951032a33d743e64f60175b24b27347059910
rafael <rafaelpaula15@hotmail.com>
2022-02-01 00:53:36 -0500

    adding files

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const app = require("./app");
 app.listen(3000, function() {
   console.log("listening on 3000");
 });
+
+// app.listen(port, () => console.log(`Server running on localhost:${port}`));
